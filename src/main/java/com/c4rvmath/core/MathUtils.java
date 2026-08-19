55c66460c73e3523967cf1a9c80386b1b85daa81
Yuri C4rv <yuricarvalho12wow@gmail.com>
2026-08-19 12:37:05 -0300

    Commit v1.1.0 - Cálculos básicos adicionado

--- src/main/java/com/c4rvmath/core/MathUtils.java
+++ src/main/java/com/c4rvmath/core/MathUtils.java
@@ -6,15 +6,53 @@ public class MathUtils {
 	//Construtor privado para evitar que usem "new MathUtils()" sem necessidade
 	private MathUtils() {
 		//Impedimento de instanciação
+	
+	/*
+	 * =======
+	 * Método de cálculos:
+	 * =======
+	 */
+	
 	}
-	//Método estático para somar
+	//Método para somar
 	public static double somar(double a, double b) {
 		return a + b;
 	}
-	//Método estático para subtrair
+	//Método para subtrair
 	public static double subtrair(double a, double b) {
 		return a - b;
 	}
+	//Método para multiplicação
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	//Método para divisão
+	public static double dividir(double a, double b) {
+		if(b == 0) {
+			throw new ArithmeticException("Erro: divisão por zero não é permitida");
+		}
+		return a/b;
+	}
+	//Método para resto da divisão
+	public static double resto(double a, double b) {
+		if(b == 0) {
+			throw new ArithmeticException("Erro: não é possível calcular o resto de uma divisão por zero!");
+		}
+		return a % b;
+	}
+	//Método para potência
+	public static double potenciar(double base, double expoente) {
+		return Math.pow(base, expoente);
+	}
+	//Método para raiz quadrada
+	public static double radiciar(double numero) {
+		if(numero < 0) {
+			throw new ArithmeticException("Erro: não é possível calcular a raiz quadrada de um número negativo!");
+		}
+		return Math.sqrt(numero);
+	}
+	
+	
 	//  ===Método de entrada personalizada===
 	/*  
 	 * 	Exibe mensagem personalizada do usuário e captura o número digitado
@@ -45,9 +83,33 @@ public class MathUtils {
 			return somar(valor1, valor2);
 		case "-":
 			return subtrair(valor1, valor2);
+		case "*":
+			return multiplicar(valor1, valor2);
+		case "/":
+			return dividir(valor1, valor2);
+		case "%":
+			return resto(valor1, valor2);
+		case "^":
+			return potenciar(valor1, valor2);
+		case "v":
+			return radiciar(valor1);
+		//case
 		default:
 			//Abordagem aplicada para bibliotecas, lança um erro claro em vez de apenas printar
 			throw new IllegalArgumentException("Erro: O operador '" + operador + "' não é suportado. Aguarde futuras atualizações");
 		}
+	}
+	public static void exibirManual() {
+			System.out.println("=== MANUAL DA BIBLIOTECA C4RVMATH ===");
+			System.out.println("Como usar o metodo calcular(valor1, operador, valor2):");
+			System.out.println("");
+			System.out.println(" + : Soma o valor1 com o valor2");
+			System.out.println(" - : Subtrai o valor2 do valor1");
+			System.out.println(" * : Multiplica o valor1 pelo valor2");
+			System.out.println(" / : Divide o valor1 pelo valor2 (Gera erro se valor2 for 0)");
+			System.out.println(" % : Calcula o resto da divisao de valor1 por valor2");
+			System.out.println(" ^ : Eleva o valor1 a potencia do valor2");
+			System.out.println(" v : Calcula a raiz quadrada do valor1 (O valor2 e ignorado)");
+			System.out.println("======================================");
 	}
 }
---
+++ src/main/java/com/c4rvmath/core/Teste.java
@@ -0,0 +1,10 @@
+package com.c4rvmath.core;
+import com.c4rvmath.core.MathUtils;
+import java.util.Scanner;
+public class Teste {
+
+	public static void main(String[] args) {
+		MathUtils.exibirManual();
+	}
+
+}
